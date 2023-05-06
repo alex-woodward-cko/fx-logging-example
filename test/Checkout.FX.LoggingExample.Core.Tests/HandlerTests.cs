759371540426429515446f73de4b112f3b509321
alex-woodward-cko <alex.woodward@checkout.com>
2023-05-06 13:10:50 +0100

    Added FluentAssertsions and Globals

---
+++ test/Checkout.FX.LoggingExample.Core.Tests/HandlerTests.cs
@@ -0,0 +1,26 @@
+﻿using Checkout.Diagnostics.Abstractions;
+using Serilog;
+
+namespace Checkout.FX.LoggingExample.Core.Tests
+{
+    public class HandlerTests
+    {
+        private IHandler _handler;
+
+        private Mock<ILogger> _mockLogger;
+        private Mock<IMetrics> _mockMetrics;
+
+        public HandlerTests()
+        {
+            _mockLogger = new Mock<ILogger>();
+            _mockMetrics = new Mock<IMetrics>();
+            _handler = new Handler(_mockLogger.Object, _mockMetrics.Object);
+        }
+
+        [Fact]
+        public void DoesExists()
+        {
+            _handler.Should().NotBeNull();
+        }
+    }
+}
